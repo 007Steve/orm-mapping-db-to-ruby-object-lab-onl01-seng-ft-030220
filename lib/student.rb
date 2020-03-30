9bbb548ae9e5149d9d0c4bc2f1dd8eba3333d5b0
Stephen Plummer <stephen.plummer1.sp@gmail.com>
2020-03-30 23:16:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -99,7 +99,16 @@ def self.first_X_students_in_grade_10(x)
       self.new_from_db(row)
 end
 end
-def self.first_student_in_grade_10
+def self.first_student_in_grade_10(x)
+    sql = <<-SQL
+    SELECT *
+  FROM students
+  WHERE grade = 10
+  LIMIT 1
+  
+    SQL
+    DB[:conn].execute(sql,x).map do |row|
+      self.new_from_db(row)
   
 end
 end
